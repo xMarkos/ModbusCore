12a20921ae6645b7a93b9499fa2f2d1f056b3243
Marek Hübsch <marek.huebsch@enpal.de>
2021-09-10 19:51:21 +0200

    Implemented writing single register request and response messages (function 0x06)

--- ModbusCore/Modbus/Messages/WriteSingleValueMessage.cs
+++ ModbusCore/Modbus/Messages/WriteSingleValueMessage.cs
@@ -2,23 +2,28 @@
 
 namespace ModbusCore.Messages
 {
-    public record WriteSingleCoilMessage : MessageBase
+    public record WriteSingleValueMessage : MessageBase
     {
         public ushort Register { get; init; }
-        public bool Value { get; init; }
+        public ushort Value { get; init; }
 
-        public WriteSingleCoilMessage() { }
+        public bool Coil
+        {
+            // By specification, only 0xFF00 is true, and 0x0000 is false, but let's parse any non-zero value as true
+            get => Value != 0;
+            init => Value = (ushort)(value ? 0xFF00 : 0x0000);
+        }
 
-        public WriteSingleCoilMessage(ReadOnlySpan<byte> buffer)
+        public WriteSingleValueMessage() { }
+
+        public WriteSingleValueMessage(ReadOnlySpan<byte> buffer)
             : base(buffer)
         {
             if (buffer.Length < 6)
                 throw new ArgumentException(null, nameof(buffer));
 
             Register = ModbusUtility.ReadUInt16(buffer[2..]);
-
-            // By specification, only 0xFF00 is true, and 0x0000 is false, but let's parse as non-zero value as true
-            Value = ModbusUtility.ReadUInt16(buffer[4..]) != 0;
+            Value = ModbusUtility.ReadUInt16(buffer[4..]);
         }
 
         public override bool TryWriteTo(Span<byte> buffer, out int length)
@@ -30,7 +35,7 @@ namespace ModbusCore.Messages
                 return false;
 
             ModbusUtility.Write(buffer[2..], Register);
-            ModbusUtility.Write(buffer[4..], Value ? 0xFF00 : 0x0000);
+            ModbusUtility.Write(buffer[4..], Value);
             return true;
         }
     }

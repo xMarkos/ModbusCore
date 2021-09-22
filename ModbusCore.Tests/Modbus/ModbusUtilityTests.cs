e84b54820d680bcefc1db80ad9177830b8a91fe6
Marek Hübsch <marek.huebsch@enpal.de>
2021-09-22 11:04:44 +0200

    Added more tests for CRC computation

--- ModbusCore.Tests/Modbus/ModbusUtilityTests.cs
+++ ModbusCore.Tests/Modbus/ModbusUtilityTests.cs
@@ -8,6 +8,10 @@ namespace ModbusCore
         [InlineData(new byte[] { 0x11, 0x03, 0x00, 0x6B, 0x00, 0x03 }, 0x8776)]
         [InlineData(new byte[] { 1, 4, 0, 101, 0, 4 }, 0xD6E1)]
         [InlineData(new byte[] { 0x02, 0x07 }, 0x1241)]
+        [InlineData(new byte[] { 0x01, 0x04, 0x00, 0x03, 0x00, 0x01 }, 0xCAC1)]
+        [InlineData(new byte[] { 0x01, 0x04, 0x02, 0xFF, 0xFF }, 0x80B8)]
+        [InlineData(new byte[] { 0x02, 0x04, 0x00, 0x03, 0x00, 0x01 }, 0xF9C1)]
+        [InlineData(new byte[] { 0x02, 0x04, 0x02, 0x7F, 0x58 }, 0xFADC)]
         public void CalculateCrc16_ReturnsCorrectChecksum(byte[] input, ushort expected)
         {
             // Note: When adding tests, the expected value bytes need to be swapped because
